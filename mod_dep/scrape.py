badb4493fd246cc736d7ec0178b50d7b4b1bf50e
Jenda Mudron <jenmud@gmail.com>
2016-07-09 16:06:48 +1000

    added in mro and started to neaten it up a bit

--- mod_dep/scrape.py
+++ mod_dep/scrape.py
@@ -1,9 +1,8 @@
-#pylint: skip-file
-
-import logging
+import argparse
 import inspect
-import ruruki
 import importlib
+import logging
+import ruruki
 
 from ruruki.graphs import Graph
 
@@ -18,10 +17,51 @@ GRAPH.add_vertex_constraint("module", "name")
 
 SEEN = set()
 
+
+def map_filename_from_module(module, parent):
+    try:
+        filename = inspect.getfile(module)
+        if filename:
+            parent_file = GRAPH.get_or_create_vertex("file", name=filename)
+            GRAPH.get_or_create_edge(parent, "found-in", parent_file)
+            logging.debug("(%s)-[:found-in]->(%s)", filename, module)
+    except TypeError:
+        logging.warn("Failed to get the file for %s", module)
+
+
+def map_functions_from_module(module, parent):
+    # get all the functions in the module
+    for fname, func in inspect.getmembers(module, inspect.isfunction):
+        fnode = GRAPH.get_or_create_vertex("function", name=fname)
+        GRAPH.get_or_create_edge(parent, "has-function", fnode)
+
+
+def map_classes_from_module(module, parent):
+    # get all the classes in the module
+    for cname, cls in inspect.getmembers(module, inspect.isclass):
+        cnode = GRAPH.get_or_create_vertex("class", name=cname)
+        GRAPH.get_or_create_edge(parent, "has-class", cnode)
+        last = cnode
+        for name in inspect.getmro(cls):
+            node = GRAPH.get_or_create_vertex("class", name=name.__name__)
+            GRAPH.get_or_create_edge(last, "subclasses", node)
+            last = node
+
+        try:
+            # get all the class methods in the class
+            for mname, meth in inspect.getmembers(cls, inspect.ismethod):
+                mnode = GRAPH.get_or_create_vertex("method", name=mname)
+                GRAPH.get_or_create_edge(cnode, "has-method", mnode)
+        except ImportError:
+            logging.exception("Skipping due to an import error")
+
+
 def build_dep(module, parent):
     previous = parent
 
     for name, module in inspect.getmembers(module, inspect.ismodule):
+
+        # avoid loops
         if module in SEEN:
             continue
         SEEN.add(module)
@@ -31,37 +71,13 @@ def build_dep(module, parent):
         # link to the previous parent
         GRAPH.get_or_create_edge(parent, "comes-from", previous)
         logging.debug(
-            "(%s)-[:comes-from]-(%s)", name, previous.properties["name"]
+            "(%s)-[:comes-from]->(%s)", name, previous.properties["name"]
         )
 
-        try:
-            filename = inspect.getfile(module)
-            if filename:
-                parent_file = GRAPH.get_or_create_vertex("file", name=filename)
-                GRAPH.get_or_create_edge(parent, "found-in", parent_file)
-                logging.debug("(%s)-[:found-in]-(%s)", filename, name)
-        except TypeError:
-            logging.warn("Failed to get the file for %s", name)
 
-
-        # get all the functions in the module
-        for fname, func in inspect.getmembers(module, inspect.isfunction):
-            fnode = GRAPH.get_or_create_vertex("function", name=fname)
-            GRAPH.get_or_create_edge(parent, "has-function", fnode)
-
-        # get all the classes in the module
-        for cname, cls in inspect.getmembers(module, inspect.isclass):
-            cnode = GRAPH.get_or_create_vertex("class", name=cname)
-            GRAPH.get_or_create_edge(parent, "has-class", cnode)
-
-            try:
-                # get all the class methods in the class
-                for mname, meth in inspect.getmembers(cls, inspect.ismethod):
-                    mnode = GRAPH.get_or_create_vertex("method", name=mname)
-                    GRAPH.get_or_create_edge(cnode, "has-method", mnode)
-            except ImportError:
-                logging.exception("Skipping due to an import error")
-
+        map_filename_from_module(module, parent)
+        map_functions_from_module(module, parent)
+        map_classes_from_module(module, parent)
         build_dep(module, parent)
 
 
@@ -76,8 +92,7 @@ def scrape(module):
         logging.exception("Hmmm, seems like something went wrong !")
 
 
-if __name__ == "__main__":
-    import argparse
+def main():
     from ruruki_eye.server import run
 
     parser = argparse.ArgumentParser()
@@ -103,6 +118,11 @@ if __name__ == "__main__":
         help="Logging level."
     )
 
+    parser.add_argument(
+        "--logfile",
+        help="Send logs to a file. Default is to log to stdout."
+    )
+
     ns = parser.parse_args()
 
     levels = {
@@ -111,7 +131,11 @@ if __name__ == "__main__":
         "error": logging.ERROR,
         "debug": logging.DEBUG,
     }
-    logging.basicConfig(level=levels.get(ns.level, logging.INFO))
+
+    logging.basicConfig(
+        filename=ns.logfile,
+        level=levels.get(ns.level, logging.INFO)
+    )
 
     scrape(ns.module)
 
@@ -119,3 +143,7 @@ if __name__ == "__main__":
     logging.info("Edges: %d", len(GRAPH.edges))
 
     run("0.0.0.0", ns.port, False, GRAPH)
+
+
+if __name__ == "__main__":
+    main()
