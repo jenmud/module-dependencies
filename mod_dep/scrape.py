711dd6584a2726deed75757c458fbb4855bb4d87
Jenda Mudron <jenmud@gmail.com>
2016-07-10 20:42:08 +1000

    added in some doco and split the run server into another function

--- mod_dep/scrape.py
+++ mod_dep/scrape.py
@@ -1,3 +1,6 @@
+"""
+Find all interesting information for a given installed module.
+"""
 import argparse
 import inspect
 import importlib
@@ -18,23 +21,42 @@ SEEN = set()
 
 
 def map_filename(obj, parent):
-    filename = inspect.getsourcefile(obj)
-    if filename:
-        node = GRAPH.get_or_create_vertex("file", name=filename)
-        GRAPH.get_or_create_edge(parent, "found-in", node)
+    """
+    Find and map all the file names that the obj comes from.
 
-        logging.debug(
-            "(%s)-[:found-in]->(%s)",
-            parent.properties["name"],
-            filename,
-        )
+    :param obj: Find all files which the object comes from.
+    :type obj: :class:`object`
+    :param parent: Parent node which you are searching in.
+    :type parent: :class:`ruruki.interfaces.IVertex`
+    """
+    try:
+        filename = inspect.getsourcefile(obj)
+        if filename:
+            node = GRAPH.get_or_create_vertex("file", name=filename)
+            GRAPH.get_or_create_edge(parent, "found-in", node)
+
+            logging.debug(
+                "(%s)-[:found-in]->(%s)",
+                parent.properties["name"],
+                filename,
+            )
+    except TypeError:
+        logging.debug("Could not find file for: %s", obj.__name__)
 
 
 def map_functions(obj, parent):
-    # get all the functions in the module
+    """
+    Find and map all the functions recursively.
+
+    :param obj: Find all functions from the object.
+    :type obj: :class:`object`
+    :param parent: Parent node which you are searching in.
+    :type parent: :class:`ruruki.interfaces.IVertex`
+    """
     for name, obj in inspect.getmembers(obj, inspect.isfunction):
         node = GRAPH.get_or_create_vertex("function", name=name)
         GRAPH.get_or_create_edge(parent, "has-function", node)
+        map_filename(obj, node)
 
         logging.debug(
             "(%s)-[:has-function]->(%s)",
@@ -44,10 +66,19 @@ def map_functions(obj, parent):
 
 
 def map_method(obj, parent):
+    """
+    Find and map all the methods recursively.
+
+    :param obj: Find all methods from the object.
+    :type obj: :class:`object`
+    :param parent: Parent node which you are searching in.
+    :type parent: :class:`ruruki.interfaces.IVertex`
+    """
     try:
         for name, obj in inspect.getmembers(obj, inspect.ismethod):
             node = GRAPH.get_or_create_vertex("method", name=name)
             GRAPH.get_or_create_edge(parent, "has-method", node)
+            map_filename(obj, node)
 
             logging.debug(
                 "(%s)-[:has-method]->(%s)",
@@ -60,9 +91,18 @@ def map_method(obj, parent):
 
 
 def map_classes(obj, parent):
+    """
+    Find and map all the classes and the methods for the class recursively.
+
+    :param obj: Find all classes from the object.
+    :type obj: :class:`object`
+    :param parent: Parent node which you are searching in.
+    :type parent: :class:`ruruki.interfaces.IVertex`
+    """
     for name, obj in inspect.getmembers(obj, inspect.isclass):
         node = GRAPH.get_or_create_vertex("class", name=name)
         GRAPH.get_or_create_edge(parent, "has-class", node)
+        map_filename(obj, node)
 
         logging.debug(
             "(%s)-[:has-class]->(%s)",
@@ -76,6 +116,7 @@ def map_classes(obj, parent):
         for name in inspect.getmro(obj)[1:]:
             node = GRAPH.get_or_create_vertex("class", name=name.__name__)
             GRAPH.get_or_create_edge(last, "subclasses", node)
+            map_filename(obj, node)
 
             logging.debug(
                 "(%s)-[:subclasses]->(%s)",
@@ -88,6 +129,14 @@ def map_classes(obj, parent):
 
 
 def map_modules(obj, parent):
+    """
+    Find and map all the modules recursively.
+
+    :param obj: Find all modules from the object.
+    :type obj: :class:`object`
+    :param parent: Parent node which you are searching in.
+    :type parent: :class:`ruruki.interfaces.IVertex`
+    """
     # get all the functions in the module
     for name, obj in inspect.getmembers(obj, inspect.ismodule):
         _id = id(obj) + id(parent)
@@ -96,6 +145,7 @@ def map_modules(obj, parent):
         SEEN.add(_id)
         node = GRAPH.get_or_create_vertex("module", name=name)
         GRAPH.get_or_create_edge(parent, "imports", node)
+        map_filename(obj, node)
 
         logging.debug(
             "(%s)-[:imports]->(%s)",
@@ -108,16 +158,44 @@ def map_modules(obj, parent):
         map_modules(obj, node)
 
 
-def scrape(obj):
-    logging.info("Scrapping %r", obj)
-    parent = GRAPH.get_or_create_vertex("module", name=obj.__name__)
-    map_modules(obj, parent)
-    map_functions(obj, parent)
+def scrape(module):
+    """
+    Start scrapping interesting information about a module.
+
+    :param module: Module that you are scrapping.
+    :type module: :types:`ModuleType`
+    """
+    logging.info("Scrapping %r", module)
+    parent = GRAPH.get_or_create_vertex("module", name=module.__name__)
+    map_filename(module, parent)
+    map_modules(module, parent)
+    map_functions(module, parent)
+
+    logging.info("Vertices: %d", len(GRAPH.vertices))
+    logging.info("Edges: %d", len(GRAPH.edges))
+    logging.info("Modules: %d", len(GRAPH.get_vertices("module")))
+    logging.info("Classes: %d", len(GRAPH.get_vertices("class")))
+    logging.info("Methods: %d", len(GRAPH.get_vertices("method")))
+    logging.info("Function: %d", len(GRAPH.get_vertices("function")))
+    logging.info("Files: %d", len(GRAPH.get_vertices("file")))
+
+
+def run_server(address="0.0.0.0", port=8000):
+    """
+    Start a web server serving up all scrapped information.
+
+    :param address: Address to bind on.
+    :type address: :class:`str`
+    :param port: Port number to listen on.
+    :type port: :class:`int`
+    """
+    run(address, port, False, GRAPH)
 
 
 def main():
-    from ruruki_eye.server import run
-
+    """
+    Command line main run.
+    """
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
@@ -166,15 +244,7 @@ def main():
     )
 
     scrape(ns.module)
-
-    logging.info("Vertices: %d", len(GRAPH.vertices))
-    logging.info("Edges: %d", len(GRAPH.edges))
-    logging.info("Modules: %d", len(GRAPH.get_vertices("module")))
-    logging.info("Classes: %d", len(GRAPH.get_vertices("class")))
-    logging.info("Methods: %d", len(GRAPH.get_vertices("method")))
-    logging.info("Function: %d", len(GRAPH.get_vertices("function")))
-    logging.info("Files: %d", len(GRAPH.get_vertices("file")))
-    run(ns.address, ns.port, False, GRAPH)
+    run_server(ns.address, ns.port)
 
 
 if __name__ == "__main__":
